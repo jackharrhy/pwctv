a3d45d7267d9f0bb085ebf14736aa5d633028b19
Jack Arthur Harrhy <jack@jackharrhy.com>
2017-12-01 13:53:30 -0330

    post-highschool cleanup

---
+++ js/config.js
@@ -0,0 +1,1 @@
+var url = "https://docs.google.com/presentation/d/19qQxY1tHxxi1iUcQaSD5RvHze06p1_rSAbfPok0YKKM/embed?start=true&amp;loop=true&amp;delayms=7000"
--- js/pwctv.js
+++ js/pwctv.js
@@ -1,107 +1,110 @@
+
 function switchContainer(toSwitchTo, optionalArg) {
-	if(current) {
-		$(current.main).fadeOut(1000);
-	}
+  if(current) {
+    $(current.main).fadeOut(1000);
+  }
 
-	$(toSwitchTo.main).fadeIn(1000);
+  $(toSwitchTo.main).fadeIn(1000);
 
-	if(optionalArg) {
-		toSwitchTo.initial(optionalArg);
-	} else {
-		toSwitchTo.initial();
-	}
-	current = toSwitchTo;
+  if(optionalArg) {
+    toSwitchTo.initial(optionalArg);
+  } else {
+    toSwitchTo.initial();
+  }
+  current = toSwitchTo;
 }
 
 var container, current;
 
 document.addEventListener('DOMContentLoaded', function(event) {
-	container = {
-		textOverlay: {
-			main: document.getElementById('textOverlayContainer'),
-			text: document.getElementById('textOverlay'),
-			curData: {text:'',time:0},
+  document.getElementById('slides').src = url;
 
-			initial: function(newData) {
-				this.curData = newData;
-				this.text.innerHTML = this.curData.text;
-				if(this.curData.time > (1000 * 30)) {
-					this.curData.time = 1000 * 30;
-				}
+  container = {
+    textOverlay: {
+      main: document.getElementById('textOverlayContainer'),
+      text: document.getElementById('textOverlay'),
+      curData: {text:'',time:0},
 
-				setTimeout(this.exit, this.curData.time * 1000);
-			},
-			exit: function() {
-				switchContainer(container.slides);
-			},
-			loop: function() {
-				// nothing...
-			}
-		},
-		slides: {
-			main: document.getElementById('slidesContainer'),
-			slide: document.getElementById('slides'),
-			footerDate: document.getElementById('footerDate'),
-			footerTime: document.getElementById('footerTime'),
+      initial: function(newData) {
+        this.curData = newData;
+        this.text.innerHTML = this.curData.text;
+        if(this.curData.time > (1000 * 30)) {
+          this.curData.time = 1000 * 30;
+        }
 
-			timeOptions: {
-				hour: '2-digit',
-				minute: '2-digit'
-			},
+        setTimeout(this.exit, this.curData.time * 1000);
+      },
+      exit: function() {
+        switchContainer(container.slides);
+      },
+      loop: function() {
+        // nothing...
+      }
+    },
+    slides: {
+      main: document.getElementById('slidesContainer'),
+      slide: document.getElementById('slides'),
+      footerDate: document.getElementById('footerDate'),
+      footerTime: document.getElementById('footerTime'),
 
-			initial: function() {
-				setTimeout(this.exit, 30000);
-			},
-			exit: function() {
-				switchContainer(container.video);
-			},
-			loop: function() {
-				var currentDate = new Date();
+      timeOptions: {
+        hour: '2-digit',
+        minute: '2-digit'
+      },
 
-				this.footerDate.innerHTML = currentDate.toDateString();
-				this.footerTime.innerHTML = currentDate.toLocaleTimeString('en-us', this.timeOptions);
-			}
-		},
-		video: {
-			main: document.getElementById('videoContainer'),
-			video: document.getElementById('video'),
-			videos: [
-				'donut',
+      initial: function() {
+        //setTimeout(this.exit, 30000);
+      },
+      exit: function() {
+        //switchContainer(container.video);
+      },
+      loop: function() {
+        var currentDate = new Date();
+
+        this.footerDate.innerHTML = currentDate.toDateString();
+        this.footerTime.innerHTML = currentDate.toLocaleTimeString('en-us', this.timeOptions);
+      }
+    },
+    /*video: {
+      main: document.getElementById('videoContainer'),
+      video: document.getElementById('video'),
+      videos: [
+        'donut',
         'keat',
         'bball',
         'grease',
         'will',
-				'skate'
-			],
+        'skate'
+      ],
 
-			initial: function() {
-				this.video.onended = function() {
-					switchContainer(container.slides);
-				}
+      initial: function() {
+        this.video.onended = function() {
+          switchContainer(container.slides);
+        }
 
-				videoString = this.videos[Math.floor(Math.random() * this.videos.length)];
-				this.video.src = 'http://jackharrhy.com/videos/pwctv/'+videoString+'.mp4';
-			},
-			loop: function() {
-				// None
-			}
-		}
-	};
+        videoString = this.videos[Math.floor(Math.random() * this.videos.length)];
+        this.video.src = 'http://jackharrhy.com/videos/pwctv/'+videoString+'.mp4';
+      },
+      loop: function() {
+        // None
+      }
+    }*/
+  };
 
-	switchContainer(container.slides);
-	loop();
-	realtimeLoop();
+  switchContainer(container.slides);
+  loop();
+  realtimeLoop();
 });
 
 function realtimeLoop() {
-	requestAnimationFrame(loop);
+  requestAnimationFrame(loop);
 }
 
 function loop() {
-	current.loop();
-	setTimeout(loop, 1000);
+  current.loop();
+  setTimeout(loop, 1000);
 }
 
 setInterval(function() {
-	window.location.reload();
+  window.location.reload();
 }, 800000);
